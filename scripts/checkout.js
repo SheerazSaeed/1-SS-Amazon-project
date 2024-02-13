4048b51faeabb2fe576462cb1620bc014f6a34a2
sheeraz <sheerazsaeed22@hotmail.co.uk>
2024-02-13 18:46:46 +0000

    delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+
 
 let cartSummaryHTML = '';
 
@@ -39,7 +40,8 @@ cartSummaryHTML +=  `
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary 
+            js-delete-link" data-product-id ="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -95,3 +97,13 @@ cartSummaryHTML +=  `
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+     
+    });
+  
+});
+
