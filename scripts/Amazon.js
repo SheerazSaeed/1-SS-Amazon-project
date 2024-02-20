df10ee35d79b1c86c49288b0b0da83f7bd95c49a
sheeraz <sheerazsaeed22@hotmail.co.uk>
2024-02-20 18:51:50 +0000

    update button in checkout page changes quantity of items

--- scripts/Amazon.js
+++ scripts/Amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import {products} from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -64,14 +64,12 @@ document.querySelector('.js-products-grid')
 const addedMessageTimeouts = {};
 
 function updateCartQuantity(params) {
-  let cartQuantity = 0;
-
-    cart.forEach((cartItem) => {
-      cartQuantity += cartItem.quantity;
-    });
+  const cartQuantity = calculateCartQuantity();
     document.querySelector('.js-cart-quantity')
     .innerHTML = cartQuantity; 
 }
+
+updateCartQuantity();
 
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
