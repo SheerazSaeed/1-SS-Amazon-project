1c54d8d5aebe851831a880212a47d2b543381076
sheeraz <sheerazsaeed22@hotmail.co.uk>
2024-02-29 18:47:12 +0000

    make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,8 @@ import {
   cart,
   removeFromCart,
   calculateCartQuantity,
-  updateQuantity
+  updateQuantity,
+  updateDeliveryOption
 } from '../data/cart.js';
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
@@ -113,7 +114,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
       html+= `
-       <div class="delivery-option">
+       <div class="delivery-option js-delivery-option"
+        data-product-id = "${matchingProduct.id}"
+        data-delivery-option-id = "${deliveryOption.id}">
         <input type="radio"
           ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
@@ -193,3 +196,11 @@ document.querySelectorAll('.js-update-link')
       updateCartQuantity();
     });
   });
+
+  document.querySelectorAll('.js-delivery-option')
+    .forEach((element)=>{
+      element.addEventListener('click', ()=>{
+        const {productId, deliveryOptionId} = element.dataset;
+        updateDeliveryOption(productId, deliveryOptionId);
+      });
+    });
